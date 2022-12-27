4f8f639c77ed814527ae447bc984b51a35e34662
TMillsap04 <tmmillsap@student.fullsail.edu>
2022-12-26 19:44:22 -0600

    Starting Hero Section

---
+++ portfolio-project/src/components/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <div className='text-center text-3xl text-pink-700'>
+        Hello this is Tatiana Millsap
+    </div>
+  )
+}
+
+export default HeroSection
